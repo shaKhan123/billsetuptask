a73bc3692d77d46678aa1da16467cb147d3195ec
Shagufta Khan <ksha786000@gmail.com>
2020-07-15 23:17:40 +0530

    first commit

---
+++ src/test/java/io/spring/billsetuptask/BillsetuptaskApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.billsetuptask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillsetuptaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
